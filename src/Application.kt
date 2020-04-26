f51469f2b229d29533584510ab2d892346ae3d26
Lukas Sztefek <lukas.sztefek@gmail.com>
2020-04-26 11:51:00 +0200

    Install & intercept

--- src/Application.kt
+++ src/Application.kt
@@ -3,23 +3,47 @@
 package cz.skywall.microfunspace
 
 import io.ktor.application.*
+import io.ktor.features.CallLogging
+import io.ktor.features.ContentNegotiation
+import io.ktor.features.DefaultHeaders
+import io.ktor.gson.gson
+import io.ktor.request.uri
 import io.ktor.response.*
 import io.ktor.routing.get
 import io.ktor.routing.routing
 import io.ktor.server.engine.commandLineEnvironment
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
+import java.text.DateFormat
+import java.util.*
 
 fun main(args: Array<String>) {
     embeddedServer(Netty, commandLineEnvironment(args)).start(wait = true)
 }
 
 fun Application.module() {
+    install(DefaultHeaders)
+    install(CallLogging)
+    install(ContentNegotiation) {
+        gson {
+            setDateFormat(DateFormat.FULL)
+            setPrettyPrinting()
+        }
+    }
+
+    intercept(ApplicationCallPipeline.Features) {
+        if (call.request.uri.contains("admin")) {
+            call.respondText("No way!")
+            finish()
+        }
+        proceed()
+    }
 
     routing {
         get("/") {
-            call.respondText("Hello World!")
+            call.respond(Text("Hello World!", Date.from(Calendar.getInstance().toInstant())))
         }
     }
 }
 
+data class Text(val text: String, val date: Date)
